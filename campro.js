db5e7dfd27ecabbaddbffb7647a319a5cb3c8ba5
syhros <79529557+syhros@users.noreply.github.com>
2025-05-23 07:30:47 +0100

    Update campro.js

--- campro.js
+++ campro.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         CamPRO - WIMS Enhancer
 // @namespace    http://tampermonkey.net/
-// @version      0.2.016.19
+// @version      0.2.016.22
 // @description  Streamlines WIMS case management with quick action buttons
 // @author       camrees
 // @match        https://optimus-internal-eu.amazon.com/*
